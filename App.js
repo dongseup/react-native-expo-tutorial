f4b158ef2501dccd2113567dea14cfcc2286f950
dongseup_lim <dongseup83@gmail.com>
2023-03-07 23:20:43 +0900

    제스쳐 추가

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import IconButton from './components/IconButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -49,7 +50,7 @@ export default function App() {
     };
 
     return (
-        <View style={styles.container}>
+        <GestureHandlerRootView style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer
                     placeholderImageSource={PlaceholderImage}
@@ -95,7 +96,7 @@ export default function App() {
                 />
             </EmojiPicker>
             <StatusBar style="auto" />
-        </View>
+        </GestureHandlerRootView>
     );
 }
 
